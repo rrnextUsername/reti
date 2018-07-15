8e70cccbfb86c9cb6c9f1ba194e4a6b04c95bf58
rrnextUsername <mattia.piretti@studio.unibo.it>
2018-07-15 11:35:20 +0200

    Add files via upload

---
+++ custom_2,4.py
@@ -0,0 +1,33 @@
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+
+        s1 = self.addSwitch( 's1' )
+	s2 = self.addSwitch( 's2' )
+
+	s3 = self.addSwitch( 's_12' )
+        s4 = self.addSwitch( 's_21' )
+
+        # Add links
+        self.addLink( h1, s1 )
+        self.addLink( h2, s2 )
+
+	self.addLink( s1, s3 )
+	self.addLink( s3, s2 )
+
+	self.addLink( s1, s4 )
+	self.addLink( s4, s2 )
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
